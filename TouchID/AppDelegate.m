f4ce907d3929ffb68e9dcc376844e91862bde6bb
wangdehuai <wangdehuai@me.com>
2014-12-03 22:00:52 +0800

    fix

--- TouchID/AppDelegate.m
+++ TouchID/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    [self.window setRootViewController:[RootViewController new]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ TouchID/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  TouchID
+//
+//  Created by wangdehuai on 14/12/3.
+//  Copyright (c) 2014年 wangdehuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
